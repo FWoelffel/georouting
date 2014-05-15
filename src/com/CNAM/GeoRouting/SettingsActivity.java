3991ca1818d192f1f80a1b99547c17063cb2cf5e
Brice FALLER <faller.brice@gmail.com>
2014-05-15 13:50:27 +0200

    Improved LoginActivity's inputs

--- src/com/CNAM/GeoRouting/SettingsActivity.java
+++ src/com/CNAM/GeoRouting/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.CNAM.GeoRouting;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,10 @@ public class SettingsActivity extends Activity implements Preferences{
             @Override
             public void onClick(View view) {
                 // TODO Launch Calendar Chooser
+            	Uri.Builder builder = Uri.parse("content://com.android.calendar/time/").buildUpon();
+        		ContentUris.appendId(builder, System.currentTimeMillis());
+        		Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+        		startActivity(intent);
             }
         });
     }
