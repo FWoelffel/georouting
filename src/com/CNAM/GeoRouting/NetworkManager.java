68d3e03428d2f299497520c691fb0d3f9653937f
Frédéric Woelffel <woelffel.f@gmail.com>
2014-05-31 03:48:27 +0200

    the NetworkManager now have all the functions he needs

--- src/com/CNAM/GeoRouting/NetworkManager.java
+++ src/com/CNAM/GeoRouting/NetworkManager.java
@@ -4,62 +4,28 @@ import android.os.StrictMode;
 
 import android.util.Base64;
 
-import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import javax.net.ssl.*;
 
-import java.net.Socket;
-import java.net.URI;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-
-
-
-/**
- * Created by fwoelffel on 25/05/14.
- */
 public class NetworkManager {
 
     // ---------------------
 
     private static NetworkManager m_instance = null;
-    private static HttpClient m_client = null;
     private static String m_credential = "";
 
-    private static final String SERVER_IP = "10.1.2.75";
+    private static final String SERVER_IP = "ot2.sqanet.fr";
     private static final String PUBLIC_ROOT = "http://" + SERVER_IP + "/api/rest";
     private static final String ROOT_PATH = "https://" + SERVER_IP + "/api/rest/1.0";
     private static final String USER_COOKIE_NAME = "AlcUserId";
@@ -68,7 +34,29 @@ public class NetworkManager {
 
     private NetworkManager() {
 
-        m_client = getHttpClient();
+        try {
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            ctx.init(null, new TrustManager[]
+            {
+                new X509TrustManager() {
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[]{}; }
+                }
+            }, null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            HttpsURLConnection.setFollowRedirects(true);
+            CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ) );
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
         // TODO : Remove the two lines above and do all the networking stuff in another thread
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -96,23 +84,25 @@ public class NetworkManager {
 
     public boolean authenticate(String _login, String _password) {
 
-        HttpGet get = new HttpGet();
-        HttpResponse response = null;
-        String responseBody = "";
-        JSONObject responseBodyJSON = null;
-
-        get.setURI(URI.create("https://"+SERVER_IP+"/api/rest/authenticate?version=1.0"));
-        response = null;
-        responseBody = "";
-        responseBodyJSON = null;
-        String credential = "";
-        String authEncode = "Basic " + Base64.encodeToString(new String(_login + ":" + _password).getBytes(), Base64.NO_WRAP);
-        get.setHeader("Authorization", authEncode);
         try {
-            response = m_client.execute(get);
-            responseBody = EntityUtils.toString(response.getEntity()).replaceAll("[\n]", "");
-            responseBodyJSON = new JSONObject(responseBody);
-            credential = responseBodyJSON.getString("credential");
+
+            URL url = new URL("https://"+SERVER_IP+"/api/rest/authenticate?version=1.0");
+            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            String authEncode = "Basic " + Base64.encodeToString(new String(_login + ":" + _password).getBytes(), Base64.NO_WRAP);
+            httpURLConnection.setRequestProperty("Authorization", authEncode);
+            httpURLConnection.setInstanceFollowRedirects(true);
+
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                String responseBody = getResponseBody(httpURLConnection);
+                JSONObject responseBodyJSON = new JSONObject(responseBody);
+
+                m_credential = responseBodyJSON.getString("credential");
+
+                return true;
+            }
+
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
@@ -121,10 +111,6 @@ public class NetworkManager {
             e.printStackTrace();
         }
 
-        if (response.getStatusLine().getStatusCode() == 200) {
-            m_credential = credential;
-            return true;
-        }
         return false;
 
     }
@@ -139,18 +125,32 @@ public class NetworkManager {
 
     private boolean openSession() {
 
-        HttpPost post = new HttpPost(ROOT_PATH + "/sessions");
-        HttpResponse response = null;
-        post.setHeader("Cookie", USER_COOKIE_NAME + "=" + m_credential);
-        post.setHeader("Content-type", "application/json");
-        post.setHeader("Accept", "application/json");
-
         try {
-            // Setting POST's data
+
+            URL url = new URL(ROOT_PATH + "/sessions");
+            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Cookie",USER_COOKIE_NAME + "=" + m_credential);
+            httpURLConnection.setRequestProperty("Content-type", "application/json");
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+
             JSONObject data = new JSONObject();
             data.put("applicationName", "GeoRouting");
-            post.setEntity(new StringEntity(data.toString()));
-            response = m_client.execute(post);
+
+            byte[] outputInBytes = data.toString().getBytes("UTF-8");
+            OutputStream os = httpURLConnection.getOutputStream();
+            os.write( outputInBytes );
+            os.close();
+
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String responseBody = getResponseBody(httpURLConnection);
+                JSONObject responseBodyJSON = new JSONObject(responseBody);
+
+                //TODO : test licenses
+
+                return true;
+            }
+
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
@@ -159,34 +159,31 @@ public class NetworkManager {
             e.printStackTrace();
         }
 
-        if (response.getStatusLine().getStatusCode() == 200) {
-            return true;
-        }
         return false;
 
     }
 
     // ---------------------
 
-    private boolean closeSession() {
+    private boolean closeSession()
+    {
+        try
+        {
+            URL url = new URL(ROOT_PATH + "/sessions");
+            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("DELETE");
+            httpURLConnection.setRequestProperty("Cookie",USER_COOKIE_NAME + "=" + m_credential);
 
-        // FIXME : Seems to freeze the application... Don't know why
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_NO_CONTENT) {
+                return true;
+            }
 
-        HttpDelete delete = new HttpDelete(ROOT_PATH + "/sessions");
-        HttpResponse response = null;
-        delete.setHeader("Cookie", USER_COOKIE_NAME + "=" + m_credential);
-
-        try {
-            response = m_client.execute(delete);
-        } catch (ClientProtocolException e) {
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (response.getStatusLine().getStatusCode() == 204) {
-            return true;
-        }
         return false;
     }
 
@@ -194,18 +191,25 @@ public class NetworkManager {
 
     public JSONArray getProfiles() {
 
-        HttpGet get = new HttpGet(ROOT_PATH + "/routing/profiles");
-        HttpResponse response = null;
-        String responseBody = "";
-        JSONObject responseBodyJSON = null;
-        get.setHeader("Cookie", USER_COOKIE_NAME + "=" + m_credential);
-
         try {
+
             openSession();
-            response = m_client.execute(get);
-//            closeSession();
-            responseBody = EntityUtils.toString(response.getEntity()).replaceAll("[\n]", "");
-            responseBodyJSON = new JSONObject(responseBody);
+
+            URL url = new URL(ROOT_PATH + "/routing/profiles");
+            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("Cookie",USER_COOKIE_NAME + "=" + m_credential);
+            httpURLConnection.setRequestProperty("Content-type", "application/json");
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String responseBody = getResponseBody(httpURLConnection);
+                JSONObject responseBodyJSON = new JSONObject(responseBody);
+                return responseBodyJSON.getJSONArray("profiles");
+            }
+
+            closeSession();
+
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (ClientProtocolException e) {
@@ -214,73 +218,107 @@ public class NetworkManager {
             e.printStackTrace();
         }
 
-        if (response.getStatusLine().getStatusCode() == 200) {
-            try {
-                return responseBodyJSON.getJSONArray("profiles");
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-        return new JSONArray();
+        return null;
     }
 
     // ---------------------
 
-    private HttpClient getHttpClient() {
+    public JSONObject getRoutingState(){
+
         try {
-            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            trustStore.load(null, null);
 
-            SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
-            sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            openSession();
 
-            HttpParams params = new BasicHttpParams();
-            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+            URL url = new URL(ROOT_PATH + "/routing/state");
+            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("Cookie",USER_COOKIE_NAME + "=" + m_credential);
+            httpURLConnection.setRequestProperty("Content-type", "application/json");
+            httpURLConnection.setRequestProperty("Accept", "application/json");
 
-            SchemeRegistry registry = new SchemeRegistry();
-            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-            registry.register(new Scheme("https", sf, 443));
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String responseBody = getResponseBody(httpURLConnection);
+                JSONObject responseBodyJSON = new JSONObject(responseBody);
+                return responseBodyJSON;
+            }
 
-            ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+            closeSession();
 
-            return new DefaultHttpClient(ccm, params);
-        } catch (Exception e) {
-            return new DefaultHttpClient();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-}
+
+        return null;
+
+    }
 
     // ---------------------
 
-    private class MySSLSocketFactory extends SSLSocketFactory {
-        SSLContext sslContext = SSLContext.getInstance("TLS");
+    public int getAppliedProfileID() {
 
-        public MySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
-            super(truststore);
+        JSONObject routingState = getRoutingState();
 
-            TrustManager tm = new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
-
-                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                }
-
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-            };
-
-            sslContext.init(null, new TrustManager[] { tm }, null);
+        try {
+            return routingState.getJSONObject("appliedProfile").getInt("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return -1;
+    }
 
-        @Override
-        public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
-            return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
-        }
+    // ---------------------
 
-        @Override
-        public Socket createSocket() throws IOException {
-            return sslContext.getSocketFactory().createSocket();
+    public boolean setAppliedProfile(int _id) {
+
+        try
+        {
+            URL url = new URL(ROOT_PATH + "/routing/profiles/" + _id);
+            HttpsURLConnection httpURLConnection = (HttpsURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("PUT");
+            httpURLConnection.setRequestProperty("Cookie", USER_COOKIE_NAME + "=" + m_credential);
+            httpURLConnection.setRequestProperty("Content-type", "application/json");
+            httpURLConnection.setRequestProperty("Accept", "application/json");
+
+            JSONObject data = new JSONObject();
+            data.put("requestId", "");
+
+            byte[] outputInBytes = data.toString().getBytes("UTF-8");
+            OutputStream os = httpURLConnection.getOutputStream();
+            os.write( outputInBytes );
+            os.close();
+
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_NO_CONTENT) {
+                return true;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return false;
+
+    }
+
+    private String getResponseBody (HttpsURLConnection _httpURLConnection) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(_httpURLConnection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
