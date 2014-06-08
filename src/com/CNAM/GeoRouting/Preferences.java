a3e456dc785f029ff0b49241695b715850324b0f
Frédéric Woelffel <woelffel.f@gmail.com>
2014-06-08 10:07:56 +0200

    added few preferences keys

--- src/com/CNAM/GeoRouting/Preferences.java
+++ src/com/CNAM/GeoRouting/Preferences.java
@@ -9,6 +9,13 @@ public interface Preferences {
     public final static String CREDENTIAL       = "credential";
     public final static String CALENDAR         = "calendar";
 
+    public final static String CALENDAR_EVENING         = "calendar_evening";
+    public final static String CALENDAR_WEEKEND         = "calendar_weekend";
+    public final static String CALENDAR_BUSY            = "calendar_busy";
+
+    public final static String GPS_LAT         = "gps_lat";
+    public final static String GPS_LON         = "gps_lon";
+
     public void loadPreferences();
     public void savePreferences();
 }
